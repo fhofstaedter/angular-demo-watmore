7e02572752c4f13105ac74b7ff37639af5b35586
Frank Hofstaedter <fhofstaedter@gmail.com>
2023-05-04 16:26:39 -0400

    Login Form, Authentication & Route Guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
 
 import { AppRoutingModule } from './app-routing.module';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home';
 import { LoginComponent, RegisterComponent } from './account';
@@ -9,6 +15,8 @@ import { LoginComponent, RegisterComponent } from './account';
 @NgModule({
     imports: [
         BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,        
         AppRoutingModule
     ],
     declarations: [
@@ -17,6 +25,12 @@ import { LoginComponent, RegisterComponent } from './account';
         LoginComponent,
         RegisterComponent
     ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],    
     bootstrap: [AppComponent]
 })
 export class AppModule { }
